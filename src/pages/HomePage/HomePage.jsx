a5a1b949349b3baaf22881caef62d86769e63411
Gastón Camú <camugaston2002@gmail.com>
2024-09-17 20:31:11 -0300

    fix4

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,26 +2,6 @@ import React, { useState } from 'react';
 import s from './HomePage.module.css';
 import { useAuth } from '../../context';
 
-import videoCerrarSesion from '../../assets/video/i-cerrarSesion.mp4';
-import videoNavegacion from '../../assets/video/i-navegacion.mp4';
-import videoCargando from '../../assets/video/i-cargando.mp4';
-import videoAlertSuccess from '../../assets/video/i-alertSuccess.mp4';
-import videoAlertError from '../../assets/video/i-alertError.mp4';
-
-import obtenerEmpleado1 from '../../assets/img/empleados/obtenerEmpleado1.png';
-import obtenerEmpleado2 from '../../assets/img/empleados/obtenerEmpleado2.png';
-import obtenerEmpleados1 from '../../assets/img/empleados/obtenerEmpleados1.png';
-import obtenerEmpleados2 from '../../assets/img/empleados/obtenerEmpleados2.png';
-import obtenerEmpleados3 from '../../assets/img/empleados/obtenerEmpleados3.png';
-import crearEmpleado1 from '../../assets/img/empleados/crearEmpleado1.png';
-import crearEmpleado2 from '../../assets/img/empleados/crearEmpleado2.png';
-import crearEmpleado3 from '../../assets/img/empleados/crearEmpleado3.png';
-import editarEmpleado1 from '../../assets/img/empleados/editarEmpleado1.png';
-import editarEmpleado2 from '../../assets/img/empleados/editarEmpleado2.png';
-import editarEmpleado3 from '../../assets/img/empleados/editarEmpleado3.png';
-import eliminarEmpleado1 from '../../assets/img/empleados/eliminarEmpleado1.png';
-import eliminarEmpleado2 from '../../assets/img/empleados/eliminarEmpleado2.png';
-
 const HomePage = () => {
   const { user } = useAuth();
   const [searchTerm, setSearchTerm] = useState('');
@@ -33,7 +13,7 @@ const HomePage = () => {
       content: (
         <>
           <p>Para cerrar sesión, haz clic en tu nombre de usuario, ubicado en la esquina superior derecha de la barra de navegación. Luego selecciona "Cerrar sesión".</p>
-          <video src={videoCerrarSesion} autoPlay loop muted className={s.video} />
+          <video src="/assets/video/i-cerrarSesion.mp4" autoPlay loop muted className={s.video} />
         </>
       )
     },
@@ -43,7 +23,7 @@ const HomePage = () => {
       content: (
         <>
           <p>Para navegar entre las secciones de la aplicación, haz clic en el ícono del menú y selecciona la página que deseas visitar. Luego, cierra el menú.</p>
-          <video src={videoNavegacion} autoPlay loop muted className={s.video} />
+          <video src="/assets/video/i-navegacion.mp4" autoPlay loop muted className={s.video} />
         </>
       )
     },
@@ -54,9 +34,9 @@ const HomePage = () => {
         <>
           <ol>
             <li>Introduce el documento del empleado en el buscador.</li>
-            <img src={obtenerEmpleado1} alt="Buscador de empleados" className={s.image} />
+            <img src="/assets/img/empleados/obtenerEmpleado1.png" alt="Buscador de empleados" className={s.image} />
             <li>Presiona el botón "Buscar".</li>
-            <img src={obtenerEmpleado2} alt="Botón buscar" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/obtenerEmpleado2.png" alt="Botón buscar" className={`${s.image} ${s.btnImage}`} />
           </ol>
         </>
       )
@@ -69,14 +49,14 @@ const HomePage = () => {
           <h2>Opción 1:</h2>
           <ol>
             <li>Presiona el botón "Mostrar Todo".</li>
-            <img src={obtenerEmpleados1} alt="Botón mostrar todo" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/obtenerEmpleados1.png" alt="Botón mostrar todo" className={`${s.image} ${s.btnImage}`} />
           </ol>
           <h2>Opción 2:</h2>
           <ol>
             <li>Deja vacío el campo de búsqueda.</li>
-            <img src={obtenerEmpleados2} alt="Buscador vacío" className={s.image} />
+            <img src="/assets/img/empleados/obtenerEmpleados2.png" alt="Buscador vacío" className={s.image} />
             <li>Presiona el botón "Buscar".</li>
-            <img src={obtenerEmpleados3} alt="Botón buscar" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/obtenerEmpleados3.png" alt="Botón buscar" className={`${s.image} ${s.btnImage}`} />
           </ol>
         </>
       )
@@ -88,11 +68,11 @@ const HomePage = () => {
         <>
           <ol>
             <li>Presiona el botón "Crear Empleado".</li>
-            <img src={crearEmpleado1} alt="Botón crear empleado" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/crearEmpleado1.png" alt="Botón crear empleado" className={`${s.image} ${s.btnImage}`} />
             <li>Completa el formulario.</li>
-            <img src={crearEmpleado2} alt="Formulario de creación de empleado" className={s.image} />
+            <img src="/assets/img/empleados/crearEmpleado2.png" alt="Formulario de creación de empleado" className={s.image} />
             <li>Presiona el botón "Crear Empleado".</li>
-            <img src={crearEmpleado3} alt="Botón para crear empleado" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/crearEmpleado3.png" alt="Botón para crear empleado" className={`${s.image} ${s.btnImage}`} />
           </ol>
         </>
       )
@@ -104,11 +84,11 @@ const HomePage = () => {
         <>
           <ol>
             <li>Presiona el botón "Editar" junto al empleado.</li>
-            <img src={editarEmpleado1} alt="Botón editar empleado" className={`${s.image} ${s.iconImage}`} />
+            <img src="/assets/img/empleados/editarEmpleado1.png" alt="Botón editar empleado" className={`${s.image} ${s.iconImage}`} />
             <li>Modifica los campos necesarios en el formulario.</li>
-            <img src={editarEmpleado2} alt="Formulario de edición" className={s.image} />
+            <img src="/assets/img/empleados/editarEmpleado2.png" alt="Formulario de edición" className={s.image} />
             <li>Presiona el botón "Guardar Cambios".</li>
-            <img src={editarEmpleado3} alt="Botón guardar cambios" className={`${s.image} ${s.btnImage}`} />
+            <img src="/assets/img/empleados/editarEmpleado3.png" alt="Botón guardar cambios" className={`${s.image} ${s.btnImage}`} />
           </ol>
         </>
       )
@@ -120,9 +100,9 @@ const HomePage = () => {
         <>
           <ol>
             <li>Presiona el botón "Eliminar" junto al empleado.</li>
-            <img src={eliminarEmpleado1} alt="Botón eliminar empleado" className={`${s.image} ${s.iconImage}`} />
+            <img src="/assets/img/empleados/eliminarEmpleado1.png" alt="Botón eliminar empleado" className={`${s.image} ${s.iconImage}`} />
             <li>Confirma la eliminación presionando "OK".</li>
-            <img src={eliminarEmpleado2} alt="Confirmación de eliminación" className={s.image} />
+            <img src="/assets/img/empleados/eliminarEmpleado2.png" alt="Confirmación de eliminación" className={s.image} />
           </ol>
         </>
       )
@@ -133,7 +113,7 @@ const HomePage = () => {
       content: (
         <>
           <p>Pantalla que se muestra cuando se está cargando alguna información.</p>
-          <video src={videoCargando} autoPlay loop muted className={s.video} />
+          <video src="/assets/video/i-cargando.mp4" autoPlay loop muted className={s.video} />
         </>
       )
     },
@@ -144,11 +124,11 @@ const HomePage = () => {
         <>
           <p><span>Acción exitosa</span></p>
           <p>Notificación que aparece cuando la acción que se realizó se efectuó exitosamente.</p>
-          <video src={videoAlertSuccess} autoPlay loop muted className={s.video} />
+          <video src="/assets/video/i-alertSuccess.mp4" autoPlay loop muted className={s.video} />
 
           <p><span>Acción errónea</span></p>
           <p>Notificación que aparece cuando la acción que se realizó falló.</p>
-          <video src={videoAlertError} autoPlay loop muted className={s.video} />
+          <video src="/assets/video/i-alertError.mp4" autoPlay loop muted className={s.video} />
         </>
       )
     },
